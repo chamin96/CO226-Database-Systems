4af402c1a7fb01c02f93fac1f715a3ab16ab3a29
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2018-12-31 19:59:21 +0530

    minor changes to folders

---
+++ lab02/variables.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<?php
+    $var = 'Bob';
+    $Var = 'Joe';
+    $bar = $var;
+    $bar = "My name is also $bar";
+    $text1 = "Hello!";
+    $text2 = "What a gloomy day!";
+    $x = 5;
+    $y = 10;
+
+
+    echo "$var, $Var <br>";
+    echo $bar. "<br>";
+    echo $text1 . " " .$text2 ."<br>";
+    echo $x + $y;
+    ?>
+</body>
+</html>
